f2b116dfdb8b4ba0db46b0ce4e79ffdef1f8623c
MarcMatten <marc@matten-online.de>
2022-06-21 18:21:27 +0200

    added comments and clean-up

--- ShiftRegister/ShiftRegister.ino
+++ ShiftRegister/ShiftRegister.ino
@@ -1,46 +1,45 @@
-int ioSelect = 1;    // SR Pin 15.
-int clockPulse = 0;  //SR Pin 7. 
-int dataOut = 2;     //SR Pin 13.
+// Pin for shift register
+int NPinIOSelect = 1;    // SR Pin 15.
+int NPinClockPulse = 0;  //SR Pin 7. 
+int NPinDataOut = 2;     //SR Pin 13.
 
-int j;               //used in a for loop to declare which bit is set to 1
-int value;           //stores the digital read value of the data pin 
-                     //(0 if no button is pressed, 1 if a button is pressed)
-int NState[8];
-
+// globals for thumb wheels
 int NThumbWheelMapL[3] = {1, 2, 3};
 int NThumbWheelMapR[3] = {7, 6, 5};
 int NThumbWheelOldL = 0;
 int NThumbWheelOldR = 0;
-
 int NThumbWheelErrorL = 0;
 int NThumbWheelErrorR = 0;
-
 bool BThumbWheelInit = false;
 bool BThumbWheelError = false;
 
-byte switchVar = 0;  //stores a byte array to show which button was pressed
-
+// execution time calculaiton
 unsigned long tExec = 0; // 20.06.2022: 145 µs
 
 void setup() {
-  pinMode(ioSelect, OUTPUT);
-  pinMode(clockPulse, OUTPUT);
-  pinMode(dataOut, INPUT);  
+  pinMode(NPinIOSelect, OUTPUT);
+  pinMode(NPinClockPulse, OUTPUT);
+  pinMode(NPinDataOut, INPUT);  
 
   Serial.begin(9600);  //setting baud rate
 }
 
 void loop() {
   // tExec = micros();
-  uint16_t  dataIn = 0;       //Swap out byte for uint16_t or uint32_t
-  int dataIn2[] = {0, 0, 0, 0, 0, 0, 0, 0};
-  digitalWrite(ioSelect, 0);    // enables parallel inputs
-  digitalWrite(clockPulse, 0);  // start clock pin low
-  digitalWrite(clockPulse, 1);  // set clock pin high, data loaded into SR
-  digitalWrite(ioSelect, 1);    // disable parallel inputs and enable serial output 
 
-  for(j = 0; j < 8  ; j++) {         //sets integer to values 0 through 7 for all 8 bits
-      value = digitalRead(dataOut); //reads data from SR serial data out pin
+  // read in shift register states
+  uint16_t  dataIn = 0; //Swap out byte for uint16_t or uint32_t
+  int dataIn2[] = {0, 0, 0, 0, 0, 0, 0, 0};
+  int value;
+  int NState[8];
+  
+  digitalWrite(NPinIOSelect, 0);    // enables parallel inputs
+  digitalWrite(NPinClockPulse, 0);  // start clock pin low
+  digitalWrite(NPinClockPulse, 1);  // set clock pin high, data loaded into SR
+  digitalWrite(NPinIOSelect, 1);    // disable parallel inputs and enable serial output 
+
+  for(int j = 0; j < 8  ; j++) {         //sets integer to values 0 through 7 for all 8 bits
+      value = digitalRead(NPinDataOut); //reads data from SR serial data out pin
       NState[j] = value;
       if (value) {
         int a = (1 << j);       // shifts bit to its proper place in sequence. 
@@ -48,11 +47,11 @@ void loop() {
         dataIn = dataIn | a;    //combines data from shifted bits to form a single 8-bit number
                                 /*for more information see Arduino "Bitwise operators" */
         }
-        digitalWrite(clockPulse, LOW);  //after each bit is logged, 
-        digitalWrite(clockPulse, HIGH); //pulses clock to get next bit
+        digitalWrite(NPinClockPulse, LOW);  //after each bit is logged, 
+        digitalWrite(NPinClockPulse, HIGH); //pulses clock to get next bit
       }
       
-  // check for error
+  // Interpret shift register states  
   int NStateCountL = 0;
   int NStateCountR = 0;
   int NThumbWheelL = 0;
@@ -60,19 +59,18 @@ void loop() {
   
   for(int k=0; k <3; k++)
   {
+    // how many pins are high
     NStateCountL += NState[NThumbWheelMapL[k]];
     NStateCountR += NState[NThumbWheelMapR[k]];
-  
-    if (NState[NThumbWheelMapL[k]] == 1){
-      NThumbWheelL = k;
-    }
-    
-    if (NState[NThumbWheelMapR[k]] == 1){
-      NThumbWheelR = k;
-    }
+
+    // which pin is high
+    if (NState[NThumbWheelMapL[k]] == 1){ NThumbWheelL = k; }    
+    if (NState[NThumbWheelMapR[k]] == 1){ NThumbWheelR = k; }
   }
-  
+
+  // Detect Thumb Wheel Errors
   if (NStateCountL != 1 || NStateCountR != 1) {
+    // error when not exactly one pin high per thumg wheel
     BThumbWheelError = true;
     // Left Thumb Wheel Error
     if (NStateCountL != 1) { 
@@ -84,11 +82,13 @@ void loop() {
       if (NThumbWheelErrorR >= 10){ Serial.print("ERROR: Right Thumb Wheel reading "); Serial.print(NStateCountR); Serial.println(" states!");} } 
   }
   else{
+    // No Error
     BThumbWheelError = false;
     NThumbWheelErrorL = 0;
     NThumbWheelErrorR = 0;
   }
-  
+
+  // Thumb Wheel Switch detection
   if (BThumbWheelError == false) {
     if (BThumbWheelInit == true){
       if (NThumbWheelOldL == 0){
@@ -120,7 +120,7 @@ void loop() {
       NThumbWheelOldL = NThumbWheelL;
       NThumbWheelOldR = NThumbWheelR;
     }
-    else{ // initialise
+    else{ // Initialistion
       NThumbWheelOldL = NThumbWheelL;
       NThumbWheelOldR = NThumbWheelR;
       BThumbWheelInit = true;
